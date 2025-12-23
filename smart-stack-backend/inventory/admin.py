8e8e42fed94e4b5857c0a1215391901483ffa93d
27-varsha27 <pamisettyvarsha123@gmail.com>
2025-12-23 10:47:11 +0530

    Updated smart-stack-backend with latest changes

--- smart-stack-backend/inventory/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Store,Product,Stock,Transaction
-admin.site.register(Store)
-admin.site.register(Product)
-admin.site.register(Stock)
-admin.site.register(Transaction)
